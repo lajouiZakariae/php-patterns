65e70062f2fa676fcb67001977ce98314a51ae61
Lajoui Zakariae <lajoui.zakariae.1@gmail.com>
2023-12-28 13:26:01 +0100

    php unit

--- index.php
+++ index.php
@@ -1,37 +1,8 @@
 <?php
 
+use PHPPatterns\Event;
+
 require './vendor/autoload.php';
-
-
-class Event
-{
-    private $events = [];
-
-    public function on(string $event_name, callable $callback)
-    {
-        if (!isset($this->events[$event_name])) {
-            $this->events[$event_name] = [$callback];
-        }
-
-        return function () use ($event_name, $callback) {
-
-            $filter = function ($value) use ($callback) {
-                return $value !== $callback;
-            };
-
-            $this->events[$event_name] = array_filter($this->events[$event_name], $filter);
-        };
-    }
-
-    public function fire(string $event_name)
-    {
-        if (isset($this->events[$event_name]) && !empty($this->events[$event_name])) {
-            foreach ($this->events[$event_name] as $callback) {
-                $callback();
-            }
-        }
-    }
-}
 
 $event = new Event();
 
